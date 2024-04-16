5d7020e5d675a4fb3c5be1918d852e158eeeca93
slaygun <nabhraghav@gmail.com>
2024-04-16 16:18:54 +0530

    back button in upload position changed

--- src/lyfelynkMVP_frontend/src/Health-Service/sub/UploadPage.jsx
+++ src/lyfelynkMVP_frontend/src/Health-Service/sub/UploadPage.jsx
@@ -100,7 +100,7 @@ export default function UploadContent() {
   };
 
   return (
-    <div>
+    <div className="max-w-6xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
       <div className="flex flex-col items-center justify-center p-8">
         <div className="flex items-center justify-between w-full">
           <Link to="/Health-User/MyHealth">
