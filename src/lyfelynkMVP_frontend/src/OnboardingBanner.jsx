9c81f32266cfa65d8511b04c571351a4aad77439
slaygun <nabhraghav@gmail.com>
2024-04-23 01:34:58 +0530

    banner font size edit

--- src/lyfelynkMVP_frontend/src/OnboardingBanner.jsx
+++ src/lyfelynkMVP_frontend/src/OnboardingBanner.jsx
@@ -2,9 +2,9 @@ import { TriangleAlert } from "lucide-react";
 
 export default function OnboardingBanner() {
   return (
-    <div className="flex justify-center bg-primary space-x-4 py-2 px-4 text-center">
-      <TriangleAlert className="text-white"/>
-      <p className="text-white text-lg font-bold">Connect your Wallet prior to Sign Up/Login In.</p>
+    <div className="flex justify-center items-center bg-primary py-2 px-4 text-center">
+      <TriangleAlert className="text-white mr-2"/>
+      <p className="text-left text-white text-xs md:text-base md:font-medium">Connect your Wallet prior to Sign Up/Login In.</p>
     </div>
   )
 }
